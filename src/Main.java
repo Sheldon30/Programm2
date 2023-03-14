7694ca4fe7f7e351ff6381284c7062be1086cc84
Sheldon <aneln700@gmail.com>
2023-03-14 16:23:59 +0300

    ignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int start = 900;
+        int deposit = 500;
+        int bonus = deposit/100;
+        if (deposit > 1000) {
+            bonus = deposit / 100;
+        } else if (deposit < 1000) {
+            bonus = 0;
+        }
+        int finishBalance = start + bonus;
+        System.out.println("Итоговая сумма:" + finishBalance + " рублей");
     }
 }
